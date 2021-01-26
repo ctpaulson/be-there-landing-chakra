55e147ce7d7fd23667b69107759558b34197a4f5
Conner Paulson <40129475+ctpaulson@users.noreply.github.com>
2021-01-26 12:45:33 -0700

    footer improvements

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-import { Box, Text, Image, Stack, Divider, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Image,
+  Stack,
+  Divider,
+  Spacer,
+  Center
+} from "@chakra-ui/react";
 
 export default function Footer() {
   return (
@@ -10,15 +18,17 @@ export default function Footer() {
         spacing={["10", "10", "20"]}
         mb="10"
       >
-        <Box>
+        <Center>
           <Image src="/logo-white-vertical.png" h="100px"></Image>
-        </Box>
+        </Center>
         <Spacer />
         <Box>Links go here</Box>
         <Box>More links go here</Box>
       </Stack>
       <Divider />
-      <Text mt="10">Make sure everyone you love can Be There.</Text>
+      <Text mt="10">
+        &ldquo;Make sure everyone you love can Be There.&rdquo;
+      </Text>
       <Text color="gray.500">&copy; 2020, Be There Events</Text>
     </Box>
   );
